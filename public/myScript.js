b748c17f76a77dcabfb0258697a223d7243183d5
Lellisinthewoods <ellinor.romin@hotmail.com>
2022-12-11 15:30:39 +0100

    add movie function

--- public/myScript.js
+++ public/myScript.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-//import { collection, docs } from "https://www.gstatic.com/firebasejs/9.15.0/firestore-app.js";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,5 +16,23 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-console.log("hej hej!");
+console.log(app, db);
+
+let movieTitle = document.querySelector(`#movieTitle`);
+let movieGenre = document.querySelector(`#movieGenre`);
+let movieDate = document.querySelector(`#movieDate`);
+let saveBTN = document.querySelector(`#saveButton`);
+
+saveBTN.addEventListener(`click`, ()=>{
+    saveToDataBase(movieTitle.value, movieGenre.value, movieDate.value)
+})
+
+async function saveToDataBase(movieTitle, movieGenre, movieDate) {
+    await addDoc(collection(db, 'DE-VE-DE-DB'), { // Lägger till en ny todo i vår databas i vår collection todos
+        title: movieTitle,
+        genre: movieGenre,
+        released: movieDate
+    });
+}
